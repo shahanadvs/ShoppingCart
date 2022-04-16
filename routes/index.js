43b21e71e19ffe56a827559add41f12248b67606
Shahanad Vs <shahanadvs@gmail.com>
2022-04-16 23:05:03 +0530

    Created layouts and partials, added nav based on user, added cards using data from array,

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var products = [
+    {
+      'name':"Iphone",
+      "description": "Nice phone 1",
+      'img-link':"https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBob25lfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+    },
+    {
+      'name':"Iphone 2",
+      "description": "Nice phone 2",
+      'img-link':"https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBob25lfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+    },
+    {
+      'name':"Iphone 3",
+      "description": "Nice phone 3",
+      'img-link':"https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBob25lfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+    },
+    {
+      'name':"Iphone 4",
+      "description": "Nice phone 4",
+      'img-link':"https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBob25lfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+    },
+  ]
+
+
+  res.render('index', { products, admin:false });
 });
 
 module.exports = router;
